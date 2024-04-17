90a0b5539d37447bf0aaae231280bdfb6313b655
Krst-G <kerstin.guttenberger@outlook.com>
2024-04-17 12:10:06 +0200

    push

--- models/mart/mart_temps.sql
+++ models/mart/mart_temps.sql
@@ -1,12 +1,12 @@
 select city
 		,month_of_year
-		,num_month
+		,month
 		,round(avg(max_temp_c),2) as max_temp_c
 		,round(avg(min_temp_c),2) as min_temp_c
 		,round(avg(avg_temp_c),2) as avg_temp_c 
 from {{ref('mart_forecast_day')}} 
 group by city
 	,month_of_year
-	,num_month 
+	,month 
 order by city asc
-	,num_month asc
+	,month asc
