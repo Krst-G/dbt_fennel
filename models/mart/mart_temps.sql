410bde134fb1ba42337b50c50762723686b7f8a4
Krst-G <kerstin.guttenberger@outlook.com>
2024-04-18 10:22:30 +0200

    push dbt

--- models/mart/mart_temps.sql
+++ models/mart/mart_temps.sql
@@ -7,6 +7,9 @@ select date
 		,round(avg(max_temp_c),2) as max_temp_c
 		,round(avg(min_temp_c),2) as min_temp_c
 		,round(avg(avg_temp_c),2) as avg_temp_c 
+		,lat
+        ,lon
+        ,timezone_id
 from {{ref('mart_forecast_day')}} 
 group by date
 	,year
@@ -14,6 +17,9 @@ group by date
 	,month      --- group on
 	,city
 	,country	--- group on that too
+	,lat
+    ,lon
+    ,timezone_id
 order by city asc    -- optional
 	,year asc
 	,month asc     --- order by month
